1fa1def82a728d5b31342c7ee308a267877dc034
johljohl <97534501+johljohl@users.noreply.github.com>
2022-02-24 10:10:38 +0100

    Moving shapes v1.0

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ const width = 10
 const height =20 
 let currentPosition = 4   
 
+ //assign functions to keyCodes
+ function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      rotate()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
+    }
+  }
+  document.addEventListener('keyup', control)
+
+
+
+
 
 // The tetrominoes
 const lTetromino = [
@@ -48,12 +65,66 @@ const zTetromino = [
 
 
 // Radomly select Tetromino
-let random = Math.floor(Ma.random()*theTetrominoes.length)
+let random = Math.floor(Math.random()*theTetrominoes.length)
 let currentRotation = 0
 let current = theTetrominoes[random][currentRotation]
 
 
-// move the Teromino down
+// draw the shape
+function draw() {
+    current.forEach( index => ( 
+        squares[currentPosition + index].classList.add('block')
+    ))
+}
+
+// undraw the shape
+function undraw() {
+    current.forEach( index => (
+        squares[currentPosition + index].classList.remove('block')
+    ))
+}
+
+// move down the shape
+function moveDown() {
+    undraw()
+    currentPosition = currentPosition += width
+    draw()
+    //freeze()
+}
+
+// move left an prevent collisions with shapes moving left
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === -1)
+    if(!isAtRightEdge) currentPosition +=1
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) { 
+        currentPosition -=1
+    }
+    draw()
+}
+
+function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index =>(currentPosition + index) % width === 0)
+    if(!isAtLeftEdge) currentPosition -=1
+    if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+        currentPosition +=1
+    }
+    draw()
+}
+
+// rotate Tetromino
+function rotate() {
+    undraw()
+    currentRotation ++
+    if (currentRotation === current.length) {
+        currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
+
+draw()
 
 
 }
